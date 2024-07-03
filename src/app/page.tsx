32d1a6f8651b7fc7e27669901cc7104bb532bc1e
Roberto <roberto.borjas@crizz.io>
2024-07-03 16:19:45 -0500

    Fix: Cambio no procesado en el anterior commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { fetchProducts} from "@/utils/consumers";
+import { fetchProducts } from "@/utils/consumers";
 import { ProductsClientConnecter } from "@/components/ProductsClientConnecter";
 
 
